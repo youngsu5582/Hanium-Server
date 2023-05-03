98877c8037a84dcaf41aa7370572f5669c3ebb5c
youngsu5582 <yuyoungsu5582@gmail.com>
2023-05-03 22:08:37 +0900

    [Feat] 서버 연동

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { webAdaptor } from './web.adaptor';
 
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService,webAdaptor],
+  controllers: [],
+  providers: [webAdaptor],
 })
 export class AppModule {}
